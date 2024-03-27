ab0526e3751cc57fd3b11020d4f869c2da80031d
masive89 <88432549+masive89@users.noreply.github.com>
2024-03-27 19:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ LOCATION = "us-central1"
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 def create_session():
-    chat_model = TextGenerationModel.from_pretrained("text-bison")
+    chat_model = TextGenerationModel.from_pretrained("text-bison@002")
     chat = chat_model.start_chat()
     return chat
 
